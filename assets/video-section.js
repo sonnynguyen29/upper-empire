246a7b57dc31b9c8efbf67297425a6feeb3e5dfa
sonnynguyen29 <nnthanhdat97@gmail.com>
2025-02-21 09:20:14 +0700

    Adjust 18 Feb 2025

--- assets/video-section.js
+++ assets/video-section.js
@@ -4,6 +4,8 @@ class StoryVideo extends HTMLElement {
 
         this.storyVideos = [];
         this.currentVideoIndex = 0;
+        this.touchStartX = 0;
+        this.touchEndX = 0;
     }
 
     getVideoElement() {
@@ -46,26 +48,25 @@ class StoryVideo extends HTMLElement {
                 bar.style.visibility = 'hidden';
                 progressBarWrappers[i].style.background = '#6e6e6e';
 
-                if ( i < index) { 
+                if (i < index) {
                     progressBarWrappers[i].style.background = '#6e6e6e';
                     bar.style.width = '100%';
-                } else { 
+                } else {
                     progressBarWrappers[i].style.background = '#d3d3d3';
                     bar.style.width = '0%';
                 }
-                
-                
+
                 setTimeout(() => {
                     bar.style.visibility = 'visible';
                     progressBarWrappers[i].style.background = '#d3d3d3';
-                }, 500)
+                }, 500);
             });
-        }
+        };
 
         const updateProgress = () => {
             const progress = (videoPlayer.currentTime / videoPlayer.duration) * 100;
             progressBars[this.currentVideoIndex].style.width = progress + '%';
-        }
+        };
 
         videoPlayer.addEventListener('timeupdate', updateProgress);
         videoPlayer.addEventListener('ended', () => {
@@ -75,23 +76,60 @@ class StoryVideo extends HTMLElement {
                 switchVideo(0); // Loop back to the first video
             }
         });
-            
+
         const prevVideo = () => {
             if (this.currentVideoIndex > 0) {
                 switchVideo(this.currentVideoIndex - 1);
             }
-        }
-        
+        };
+
         const nextVideo = () => {
             if (this.currentVideoIndex < this.storyVideos.length - 1) {
                 switchVideo(this.currentVideoIndex + 1);
             } else {
                 switchVideo(0); // Loop back to the first video
             }
-        }
+        };
 
         this.querySelector('#prevBtn').addEventListener('click', () => prevVideo());
         this.querySelector('#nextBtn').addEventListener('click', () => nextVideo());
+
+        // Swipe event listeners
+        const handleTouchStart = (e) => {
+            this.touchStartX = e.touches[0].clientX;
+        };
+
+        const handleTouchMove = (e) => {
+            this.touchEndX = e.touches[0].clientX;
+        };
+
+        const handleTouchEnd = () => {
+            if (this.touchStartX - this.touchEndX > 50) {
+                nextVideo(); // Swipe left-to-right
+            } else if (this.touchEndX - this.touchStartX > 50) {
+                prevVideo(); // Swipe right-to-left
+            }
+        };
+
+        const handleMouseDown = (e) => {
+            this.touchStartX = e.clientX;
+        };
+
+        const handleMouseUp = (e) => {
+            this.touchEndX = e.clientX;
+            if (this.touchStartX - this.touchEndX > 50) {
+                nextVideo();
+            } else if (this.touchEndX - this.touchStartX > 50) {
+                prevVideo();
+            }
+        };
+
+        this.addEventListener('touchstart', handleTouchStart);
+        this.addEventListener('touchmove', handleTouchMove);
+        this.addEventListener('touchend', handleTouchEnd);
+
+        this.addEventListener('mousedown', handleMouseDown);
+        this.addEventListener('mouseup', handleMouseUp);
 
         switchVideo(0);
     }
